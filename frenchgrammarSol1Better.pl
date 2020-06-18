727330559f3f4dc26dfe8fa3b6e8346606d1b5b3
sokhna09519 <67080245+sokhna09519@users.noreply.github.com>
2020-06-18 14:33:51 +0100

    Add files via upload

---
+++ frenchgrammarSol1Better.pl
@@ -0,0 +1,14 @@
+p(X,Y) :- sn(X,U), sv(U,Y).
+sn(X,Y) :- det(X,U), n(U,Y).
+sv(X,Y) :- v(X,Y).
+sv(X,Y) :- v(X,U), sn(U,Y).
+det([le|X],X).
+det([la|X],X).
+n([souris|X],X).
+n([chat|X],X).
+v([mange|X],X).
+v([trottine|X],X).
+
+%queries
+
+p(X, []).
